da5bac860939519d234ce3e0f823d054fac46bba
t8z3o9n5 <t8z3o9n5@ya.ru>
2024-01-18 18:55:13 +0300

    KS-1-4 Solving problem 4 for lesson 1

---
+++ src/main/kotlin/lessons_1/lesson1-task4.kt
@@ -0,0 +1,17 @@
+package lessons_1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327_000
+
+    println("Расстояние: $length")
+    println("Возраст: $age")
+    println("Часть дня: $partOfDay")
+    println("Секунды: $seconds")
+    println("Часть года: $partOfYear")
+    println("Апогей орбиты: $apogee")
+}
