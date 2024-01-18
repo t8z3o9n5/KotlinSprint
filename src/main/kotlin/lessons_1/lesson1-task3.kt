45f6c49f568469c04507edc0c53f64a9d680bbe2
t8z3o9n5 <t8z3o9n5@ya.ru>
2024-01-18 17:59:28 +0300

    KS-1-3 Solving problem 3 for lesson 1

---
+++ src/main/kotlin/lessons_1/lesson1-task3.kt
@@ -0,0 +1,17 @@
+package lessons_1
+
+fun main() {
+    val year = 1961
+
+    val riseHour = 9
+    val riseMinute = 7
+
+    println("Год полёта: $year")
+    println("Час взлёта: $riseHour")
+    println("Минута взлёта: $riseMinute")
+
+    val boardingHour = 10
+    val boardingMinute = 55
+
+    println("Время посадки: $boardingHour:$boardingMinute")
+}
