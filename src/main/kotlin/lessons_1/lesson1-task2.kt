88b48be958d92c72b7c1cf1e97bd9616bbb8ca30
t8z3o9n5 <t8z3o9n5@ya.ru>
2024-01-18 16:49:46 +0300

    KS-1-2 Solving problem 2 for lesson 1

--- src/main/kotlin/lessons_1/lesson1-task2.kt
+++ src/main/kotlin/lessons_1/lesson1-task2.kt
@@ -10,9 +10,9 @@ fun main() {
     println("Значение переменной с количеством заказов: $numberOfOrders")
     println("Значение переменной с благодарностью: $thankMessage")
 
-    val numberOfEmployees = 2000
+    var numberOfEmployees = 2000
 //    println("Количество работников интернет-магазина: $numberOfEmployees")
 
-    val updatedNumberOfEmployees = numberOfEmployees - 1
-    println("После увольнения одного работника кол-во работников интернет-магазинастало: $updatedNumberOfEmployees")
+    numberOfEmployees = 1999
+    println("После увольнения одного работника кол-во работников интернет-магазинастало: $numberOfEmployees")
 }
